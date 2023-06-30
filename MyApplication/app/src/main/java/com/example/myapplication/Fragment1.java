dfba51fa42e0358afa91cc239fe9ff12f80c6b18
2022070319 <wisecow703@hanyang.ac.kr>
2023-06-30 19:12:58 +0900

    Assignment1

--- MyApplication/app/src/main/java/com/example/myapplication/Fragment1.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Fragment1.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.util.JsonReader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,27 +12,63 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 import java.util.ArrayList;
 
 public class Fragment1 extends Fragment {
     private NumberDataAdapter numberDataAdapter;
     private ArrayList<NumberData> numberDatas;
+    private RecyclerView numberRecyclerView;
+
+
+    private String loadJsonFile() {
+        String json = null;
+        try {
+            InputStream inputStream = getActivity().getAssets().open("jsons/example.json");
+            int size = inputStream.available();
+            byte[] buffer = new byte[size];
+            inputStream.read(buffer);
+            inputStream.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return json;
+    }
+
+
     @Nullable
     @Override
-
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment1, container, false);
 
-        numberDatas = new ArrayList<>();
-        numberDatas.add(new NumberData("윤현우", "010-7149-9924"));
-        numberDatas.add(new NumberData("김철수", "010-0000-1111"));
-        numberDatas.add(new NumberData("김이나", "010-1234-5678"));
+        ArrayList<NumberData> listItems = new ArrayList<>();
+        try {
+            JSONObject jsonObject = new JSONObject(loadJsonFile());
+            JSONArray jsonArray = jsonObject.getJSONArray("numbers");
+            for(int i = 0;i<jsonArray.length();i++){
+                JSONObject obj;
+                obj = jsonArray.getJSONObject(i);
+                String name = obj.getString("name");
+                String number = obj.getString("number");
+                NumberData listItem;
 
-
-        RecyclerView numberRecyclerView = view.findViewById(R.id.recyclerView);
-        numberDataAdapter = new NumberDataAdapter(numberDatas);
+                listItem = new NumberData(name, number);
+                listItems.add(listItem);
+            }
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        numberRecyclerView = view.findViewById(R.id.recyclerView);
+        numberDataAdapter = new NumberDataAdapter(listItems);
+        numberRecyclerView.setLayoutManager((RecyclerView.LayoutManager) new LinearLayoutManager(getActivity()));
         numberRecyclerView.setAdapter(numberDataAdapter);
-        numberRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         return view;
     }
