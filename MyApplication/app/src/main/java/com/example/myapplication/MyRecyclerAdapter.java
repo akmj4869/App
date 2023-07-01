2305b6893468dba1d7fdcdb3cb720a086cc3050a
akmj4869 <57134776+akmj4869@users.noreply.github.com>
2023-07-01 11:06:12 +0900

    230701 11:06

--- MyApplication/app/src/main/java/com/example/myapplication/MyRecyclerAdapter.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MyRecyclerAdapter.java
@@ -1,8 +1,11 @@
 package com.example.myapplication;
 
+import android.content.Context;
+import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -19,6 +22,7 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.Vi
 
     public MyRecyclerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_item, parent, false);
+        Context context = parent.getContext();
         return new ViewHolder(view);
     }
 
@@ -27,6 +31,7 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.Vi
         numberItem item = arrayList.get(position);
         holder.name.setText(item.getName());
         holder.number.setText(item.getNumber());
+        holder.image.setImageResource(R.drawable.home);
     }
 
     @Override
@@ -37,11 +42,13 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.Vi
     public class ViewHolder extends RecyclerView.ViewHolder{
         private TextView name;
         private TextView number;
+        private ImageView image;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             name = itemView.findViewById(R.id.name);
             number = itemView.findViewById(R.id.number);
+            image = itemView.findViewById(R.id.phone);
         }
     }
 }
